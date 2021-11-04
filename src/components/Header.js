b462dfc65be42ce9b4edb06e42e80dce8cc7231a
Paulo Viana <pauloricardosmite@outlook.com>
2021-11-04 15:47:43 -0300

    alteracao links paginas no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,14 @@ export default () =>{
             <h1 className="logo">Betim Turismo</h1>
             <div id="infoInicial">
                 <tr>
-                    <td id = "tdHome"> <a href="https://naughty-brahmagupta-18bfa7.netlify.app/" id="atdHome">HOME</a> | </td>
+                    <td id = "tdHome"> <a href="https://betimturismo.netlify.app/" id="atdHome">HOME</a> | </td>
                     <td id = "tdServ"> SERVIÇOS |</td>
-                    <td id = "Cont"> <a href="https://naughty-brahmagupta-18bfa7.netlify.app/#infoContato" id="aCont">CONTATO</a> |</td>
+                    <td id = "Cont"> <a href="https://betimturismo.netlify.app/#infoContato" id="aCont">CONTATO</a> |</td>
                     <td id = "acess"> ACESSIBILIDADE</td>
                 </tr>
                 <div className="cadastro--login">
-                    <div className="cadastre"><a href="https://naughty-brahmagupta-18bfa7.netlify.app/signup" id="aCad">Cadastre-se</a> |</div>
-                    <div className="login"><a href="https://naughty-brahmagupta-18bfa7.netlify.app/login" id="aLog">Login</a></div>
+                    <div className="cadastre"><a href="https://betimturismo.netlify.app/signup" id="aCad">Cadastre-se</a> |</div>
+                    <div className="login"><a href="https://betimturismo.netlify.app/login" id="aLog">Login</a></div>
                 </div>
             </div>   
         </header>                   
