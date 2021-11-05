7c3c9c21fca7729ed3b7a8ea507f04946a6cebab
Paulo Viana <pauloricardosmite@outlook.com>
2021-11-05 10:09:08 -0300

    alteracao no login

--- src/components/User/Signup.js
+++ src/components/User/Signup.js
@@ -5,12 +5,12 @@ import Axios from 'axios';
 export default () =>{
 
     const [email, setEmail] = useState("");
-    const [usuario, setUsuario] = useState("");
+    const [login, setLogin] = useState("");
     const [senha, setSenha] = useState("");
 
     const submitCadastrar = () =>{
         Axios.post("https://betim-turismo-backend.herokuapp.com/api/insert", {
-            usuario: usuario,
+            login: login,
             senha: senha,
             email: email
         }).then(() =>{
@@ -23,7 +23,7 @@ export default () =>{
             <form> 
                 <h4>Cadastre-se</h4>
                 <input type="text" className="cadEmail" placeholder="Email" onChange={(e) => {setEmail(e.target.value);}} />
-                <input type="text" className="cadUsuario" placeholder="Nome de Usuário" onChange={(e) => {setUsuario(e.target.value);}} />
+                <input type="text" className="cadUsuario" placeholder="Nome de Usuário" onChange={(e) => {setLogin(e.target.value);}} />
                 <input type="password" className="cadSenha" placeholder="Senha" onChange={(e) => {setSenha(e.target.value);}} />
                 <input type="password" className="cadSenha--confirm" placeholder="Confirmar Senha" />
                 <input type="submit" value="Cadastrar" onClick={submitCadastrar} />
